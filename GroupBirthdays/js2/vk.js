1e507ecbe138fcc3c5b7aa7995032a8689f1b8eb
dizer1983@gmail.com <dizer1983@gmail.com>
2015-10-13 01:14:08 +0300

    44

--- GroupBirthdays/js2/vk.js
+++ GroupBirthdays/js2/vk.js
@@ -3,11 +3,20 @@
  */
 var user_ids = [];
 
-getBirthdays();
-
 function getBirthdays() {
     //var code =  'return API.users.get({"user_ids":API.friends.getAppUsers({"v": "5.37"}), "fields": "photo_50", "v": "5.37"});'; // вернуть массив members
 
     var code = 'return API.users.search({"group_id":37239804,"birth_day":3,birth_month:10})';
-    VK.api("execute", {code: code})
+    VK.api("execute", {code: code}, function (data) {
+        if (data.response) {
+            if (data.response.length > 0) {
+                alert(data);
+            }
+        } else {
+            alert(data); // в случае ошибки выведем её
+        }
+    });
+}
+
+function dd() {
 }
