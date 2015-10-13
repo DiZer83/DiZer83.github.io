70bab43efeb49df3542cbba340fc038dc2d86bc1
dizer1983@gmail.com <dizer1983@gmail.com>
2015-10-14 01:46:07 +0300

    wdwdwd

--- GroupBirthdays/js2/vk.js
+++ GroupBirthdays/js2/vk.js
@@ -48,7 +48,7 @@ function getBirthdays() {
                 for (var i = 1; i < data.response.length; i++) {
                     $('#result').append(''
                         + '<li>'
-                        + '<strong>@' + data.response[i].uid + ' (' + data.response[i].first_name + ' ' + data.response[i].last_name + ')</strong>'
+                        + '<strong>[id' + data.response[i].uid + '|' + data.response[i].first_name + ' ' + data.response[i].last_name + ']</strong>'
                         + '</li>');
                 }
                 $('#result').append('</ol>');
