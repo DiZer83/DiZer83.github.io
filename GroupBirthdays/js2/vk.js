d769243e2a2dc23c6ab367d1fdd2e42e9a15a114
dizer1983@gmail.com <dizer1983@gmail.com>
2015-10-13 21:30:22 +0300

    vk online

--- GroupBirthdays/js2/vk.js
+++ GroupBirthdays/js2/vk.js
@@ -10,7 +10,23 @@ function getBirthdays() {
     VK.api("execute", {code: code}, function (data) {
         if (data.response) {
             if (data.response.length > 0) {
-                alert(data);
+                for (var i = 0; i < data.response.length; i++) {
+                    $('#errorK').append(''
+                        + '<li class="c-list">'
+                        + '<div class="contact-pic">'
+                        + '<p>data.response[i].id</p>'
+                        + '</div>'
+                        + '<div class="contact-details">'
+                        + '<div class="pull-left">'
+                        + '<strong>' + data.response[i].first_name + ' ' + data.response[i].last_name + '</strong>'
+                        + '<small></small>'
+                        + '</div>'
+                        + '</div>'
+                        + '<div class="clearfix"></div>'
+                        + '</div>'
+                        + '</li>');
+                }
+                alert(data.response.length);
             }
         } else {
             alert(data); // в случае ошибки выведем её
