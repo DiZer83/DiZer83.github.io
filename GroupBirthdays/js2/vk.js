6825623ac29cdad769682cd7539f6682d95ca97c
dizer1983@gmail.com <dizer1983@gmail.com>
2015-10-14 02:15:36 +0300

    215454

--- GroupBirthdays/js2/vk.js
+++ GroupBirthdays/js2/vk.js
@@ -47,7 +47,7 @@ function getBirthdays() {
                 $('#result').html('');
                 $('#result').append(d + '.' + m + ' ' + 'Именинников всего: ' + (data.response.length - 1));
                 for (var i = 1; i < data.response.length; i++) {
-                    text = text + '[id' + data.response[i].uid + '|' + data.response[i].first_name + ' ' + data.response[i].last_name + ']?';
+                    text = text + '[id' + data.response[i].uid + '|' + data.response[i].first_name + ' ' + data.response[i].last_name + '] ';
                 }
                 $('#result').val(text);
             }
