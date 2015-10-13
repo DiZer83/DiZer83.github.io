003d52a06d6d4a51ab012b728291a7e732c57fb9
dizer1983@gmail.com <dizer1983@gmail.com>
2015-10-13 23:22:39 +0300

    dwdw

--- GroupBirthdays/js2/vk.js
+++ GroupBirthdays/js2/vk.js
@@ -8,6 +8,31 @@ VK.init(
     }
 );
 
+function autosize(width) {
+    //Проверяем элемент body на наличие.
+    if (!document.getElementById('body')) {
+        alert('error');
+        return;
+    }
+    // Успешно ли подключен ВК скрипт
+    if (typeof VK.callMethod != 'undefined') {
+        /*
+         Вызываем функцию vk js api для управления окном.
+         VK.callMethod('функция', параметры)
+         В данном случае у нас - VK.callMethod('изменение_размеров_окна', ширина, высота);
+         Так же добавляем еще 60 пикселей что бы было небольшое расстояние.
+         */
+        VK.callMethod('resizeWindow', 840, document.getElementById('body').clientHeight + 60);
+    } else {
+        alert('error #2');
+    }
+}
+$(document).ready(function () {
+        //Вызываем функцию регулировки высоты каждые пол секунды.
+        setInterval('autosize(607)', 500);
+    }
+);
+
 function getBirthdays() {
 
     var code = 'return API.users.search({"group_id":"37239804","birth_day":"3","birth_month":"10"});';
@@ -16,23 +41,15 @@ function getBirthdays() {
         if (r.response) {
             if (r.response.length > 0) {
                 $('#result').html('');
+                $('#result').append('Именинников всего: ' + r.response.length);
                 for (var i = 0; i < r.response.length; i++) {
                     $('#result').append(''
                         + '<li class="c-list">'
-                        + '<div class="contact-pic">'
-                        + '<p>data.response[i].id</p>'
-                        + '</div>'
-                        + '<div class="contact-details">'
-                        + '<div class="pull-left">'
-                        + '<strong>' + r.response[i].first_name + ' ' + r.response[i].last_name + '</strong>'
-                        + '<small></small>'
-                        + '</div>'
-                        + '</div>'
-                        + '<div class="clearfix"></div>'
-                        + '</div>'
+                        + '<strong>@'+ r.response[i].id +' (' + r.response[i].first_name + ' ' + r.response[i].last_name +')'+ '</strong>'
+                        + '<br>'
                         + '</li>');
                 }
-                alert(r.response.length);
+
             }
         } else {
             alert("error"); // в случае ошибки выведем её
