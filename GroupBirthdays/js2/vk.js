66a568ae447c64b518c034e7d85970d4ef7274a1
egor <dizer1983@gmail.com>
2016-01-17 18:40:00 +0300

    ``wd

--- GroupBirthdays/js2/vk.js
+++ GroupBirthdays/js2/vk.js
@@ -60,8 +60,16 @@ function getBirthdays(imgs,separator) {
     });
 }
 
+$(document).ready(function () {
 
-function showSmile() {
-    document.getElementById('smile').style.visibility = 'visible';
-}
+    var client2 = new ZeroClipboard($("#cres"), {
+        moviePath: "ZeroClipboard.swf"
+    });
 
+    client2.on("load", function(client2) {
+        client2.on("complete", function(client2, args) {
+            $('#cres').hide(); // скрываем для наглядности кнопку
+        });
+    });
+
+};
